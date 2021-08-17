b85d63efcc4f024aa28235a35338a308177df8c4
Paweł Polerowicz <p.polerowicz@gmail.com>
2021-08-17 22:55:31 +0200

    basic project setup

---
+++ CalculatorCS/MVVM/Views/MainButtonPanelView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CalculatorCS
+{
+    public partial class MainButtonGrid : UserControl
+    {
+        public MainButtonGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
